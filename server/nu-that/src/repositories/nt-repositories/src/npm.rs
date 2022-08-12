d03b732e2a355ed23621bf87b0ce547fc5290c81
Isaac <christensenisaac1995@gmail.com>
2022-08-12 11:30:48 -0600

    Initial commit

---
+++ server/nu-that/src/repositories/nt-repositories/src/npm.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+
+let NPM_BASE_URL = "https://registry.npmjs.org";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("")
+}
